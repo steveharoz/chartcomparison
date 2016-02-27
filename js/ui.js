ca0b18bec67a684dd6b99d60fdf77f765787d3cd
Steve Haroz <steve.haroz@gmail.com>
2016-02-26 19:53:39 -0600

    begin ui

---
+++ js/ui.js
@@ -0,0 +1,13 @@
+﻿
+
+d3.select("body").on("keydown", function (k) {
+	switch (d3.event.keyCode) {
+		case 37: // left
+			console.log("left");
+			break;
+		case 39: // right
+			console.log("right");
+			break;
+		default:
+	}
+});
