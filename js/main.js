afaaf803fd2391211f1c8926cd65f1d023d1d5db
Steve Haroz <steve.haroz@gmail.com>
2016-03-07 19:47:45 -0600

    feedback (close #3)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 ﻿/// <reference path="experiment.js" />
 /// <reference path="staircase.js" />
 /// <reference path="render.js" />
+/// <reference path="bootstrap/js/jquery-1.9.1.js" />
 
 var Styles = {
 	position:        'position',
@@ -10,11 +11,26 @@ var Styles = {
 
 var experiment = new Experiment();
 experiment.makeExperiment();
-var trial = experiment.nextTrial();
-draw(trial);
+var trial;
+nextTrial();
 
 function answer(setIndex) {
+	// prevent double click
+	$('#response').hide(0);
+	// record the response
 	experiment.answer(setIndex);
+
+	// feedback
+	var correct = experiment.currentTrial.correct;
+	var symbol = correct ? '✓' : '✘';
+	$('#feedback' + setIndex).text(symbol)
+
+	setTimeout(nextTrial, correct ? 500 : 2000);
+}
+
+function nextTrial() {
 	trial = experiment.nextTrial();
 	draw(trial);
+	$('#feedback0, #feedback1').text('');
+	$('#response').show(0);
 }
