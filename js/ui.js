4638fbeb5fa0998120723dc590b348b337dccb4d
Steve Haroz <steve.haroz@gmail.com>
2016-03-08 13:06:54 -0600

    keyboard (issue #10)

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,14 @@
-﻿
+﻿/// <reference path="main.js" />
 
 d3.select("body").on("keydown", function (k) {
 	switch (d3.event.keyCode) {
 		case 37: // left
 			console.log("left");
+			answer(0);
 			break;
 		case 39: // right
 			console.log("right");
+			answer(1);
 			break;
 		default:
 	}
